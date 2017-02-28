92c510c41361471b08e8df69ebbb770a6919396e
Tomasz Mackiewicz <tomasz.mackiewicz79@gmail.com>
2017-02-28 10:10:52 +0100

    SQL Dump

--- dump.sql
+++ dump.sql
@@ -1,19 +1,277 @@
-CREATE TABLE boxes (id INT AUTO_INCREMENT NOT NULL, customer_id INT DEFAULT NULL, signature VARCHAR(32) NOT NULL, status VARCHAR(3) NOT NULL, last_action DATE NOT NULL, customer_name VARCHAR(32) NOT NULL, INDEX IDX_CDF1B2E99395C3F3 (customer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
-CREATE TABLE message (id INT AUTO_INCREMENT NOT NULL, thread_id INT DEFAULT NULL, sender_id INT DEFAULT NULL, body LONGTEXT NOT NULL, created_at DATETIME NOT NULL, INDEX IDX_B6BD307FE2904019 (thread_id), INDEX IDX_B6BD307FF624B39D (sender_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
-CREATE TABLE customers (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, username_canonical VARCHAR(180) NOT NULL, email VARCHAR(180) NOT NULL, email_canonical VARCHAR(180) NOT NULL, enabled TINYINT(1) NOT NULL, salt VARCHAR(255) DEFAULT NULL, password VARCHAR(255) NOT NULL, last_login DATETIME DEFAULT NULL, confirmation_token VARCHAR(180) DEFAULT NULL, password_requested_at DATETIME DEFAULT NULL, roles LONGTEXT NOT NULL COMMENT '(DC2Type:array)', name VARCHAR(64) NOT NULL, address VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_62534E2192FC23A8 (username_canonical), UNIQUE INDEX UNIQ_62534E21A0D96FBF (email_canonical), UNIQUE INDEX UNIQ_62534E21C05FB297 (confirmation_token), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
-CREATE TABLE fees (id INT AUTO_INCREMENT NOT NULL, customer_id INT DEFAULT NULL, delivery INT NOT NULL, import INT NOT NULL, storage INT NOT NULL, UNIQUE INDEX UNIQ_A093C16C9395C3F3 (customer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
-CREATE TABLE thread_metadata (id INT AUTO_INCREMENT NOT NULL, thread_id INT DEFAULT NULL, participant_id INT DEFAULT NULL, is_deleted TINYINT(1) NOT NULL, last_participant_message_date DATETIME DEFAULT NULL, last_message_date DATETIME DEFAULT NULL, INDEX IDX_40A577C8E2904019 (thread_id), INDEX IDX_40A577C89D1C3019 (participant_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
-CREATE TABLE message_metadata (id INT AUTO_INCREMENT NOT NULL, message_id INT DEFAULT NULL, participant_id INT DEFAULT NULL, is_read TINYINT(1) NOT NULL, INDEX IDX_4632F005537A1329 (message_id), INDEX IDX_4632F0059D1C3019 (participant_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
-CREATE TABLE actions (id INT AUTO_INCREMENT NOT NULL, customer_id INT DEFAULT NULL, box_id INT DEFAULT NULL, action VARCHAR(3) NOT NULL, date DATE NOT NULL, INDEX IDX_548F1EF9395C3F3 (customer_id), INDEX IDX_548F1EFD8177B3F (box_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
-CREATE TABLE thread (id INT AUTO_INCREMENT NOT NULL, created_by_id INT DEFAULT NULL, subject VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, is_spam TINYINT(1) NOT NULL, INDEX IDX_31204C83B03A8386 (created_by_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
-ALTER TABLE boxes ADD CONSTRAINT FK_CDF1B2E99395C3F3 FOREIGN KEY (customer_id) REFERENCES customers (id) ON DELETE CASCADE;
-ALTER TABLE message ADD CONSTRAINT FK_B6BD307FE2904019 FOREIGN KEY (thread_id) REFERENCES thread (id);
-ALTER TABLE message ADD CONSTRAINT FK_B6BD307FF624B39D FOREIGN KEY (sender_id) REFERENCES customers (id);
-ALTER TABLE fees ADD CONSTRAINT FK_A093C16C9395C3F3 FOREIGN KEY (customer_id) REFERENCES customers (id) ON DELETE CASCADE;
-ALTER TABLE thread_metadata ADD CONSTRAINT FK_40A577C8E2904019 FOREIGN KEY (thread_id) REFERENCES thread (id);
-ALTER TABLE thread_metadata ADD CONSTRAINT FK_40A577C89D1C3019 FOREIGN KEY (participant_id) REFERENCES customers (id);
-ALTER TABLE message_metadata ADD CONSTRAINT FK_4632F005537A1329 FOREIGN KEY (message_id) REFERENCES message (id);
-ALTER TABLE message_metadata ADD CONSTRAINT FK_4632F0059D1C3019 FOREIGN KEY (participant_id) REFERENCES customers (id);
-ALTER TABLE actions ADD CONSTRAINT FK_548F1EF9395C3F3 FOREIGN KEY (customer_id) REFERENCES customers (id) ON DELETE CASCADE;
-ALTER TABLE actions ADD CONSTRAINT FK_548F1EFD8177B3F FOREIGN KEY (box_id) REFERENCES boxes (id) ON DELETE CASCADE;
-ALTER TABLE thread ADD CONSTRAINT FK_31204C83B03A8386 FOREIGN KEY (created_by_id) REFERENCES customers (id);
+-- MySQL dump 10.13  Distrib 5.7.17, for Linux (x86_64)
+--
+-- Host: localhost    Database: inventory
+-- ------------------------------------------------------
+-- Server version	5.7.17-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `actions`
+--
+
+DROP TABLE IF EXISTS `actions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `actions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `box_id` int(11) DEFAULT NULL,
+  `customer_id` int(11) DEFAULT NULL,
+  `action` varchar(3) COLLATE utf8_unicode_ci NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_548F1EF9395C3F3` (`customer_id`),
+  KEY `IDX_548F1EFD8177B3F` (`box_id`),
+  CONSTRAINT `FK_548F1EF9395C3F3` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `FK_548F1EFD8177B3F` FOREIGN KEY (`box_id`) REFERENCES `boxes` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `actions`
+--
+
+LOCK TABLES `actions` WRITE;
+/*!40000 ALTER TABLE `actions` DISABLE KEYS */;
+INSERT INTO `actions` VALUES (36,40,6,'In','2017-02-26'),(37,41,6,'In','2017-02-25'),(38,42,6,'In','2017-02-26'),(39,43,6,'In','2017-02-26'),(40,44,6,'In','2017-02-25'),(41,45,6,'In','2017-02-24'),(42,45,6,'Out','2017-02-25'),(43,46,6,'In','2017-02-22'),(44,46,6,'Out','2017-02-24'),(45,46,6,'In','2017-02-25'),(46,47,7,'In','2017-02-15'),(47,47,7,'Out','2017-02-22'),(48,47,7,'In','2017-02-25'),(49,48,7,'In','2017-02-17'),(50,48,7,'Out','2017-02-21'),(51,49,7,'In','2017-02-24'),(52,50,7,'In','2017-02-09'),(53,50,7,'Out','2017-02-15'),(54,51,7,'In','2017-01-03'),(55,51,7,'Out','2017-02-28');
+/*!40000 ALTER TABLE `actions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `boxes`
+--
+
+DROP TABLE IF EXISTS `boxes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `boxes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) DEFAULT NULL,
+  `signature` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `status` varchar(3) COLLATE utf8_unicode_ci NOT NULL,
+  `last_action` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_CDF1B2E99395C3F3` (`customer_id`),
+  CONSTRAINT `FK_CDF1B2E99395C3F3` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `boxes`
+--
+
+LOCK TABLES `boxes` WRITE;
+/*!40000 ALTER TABLE `boxes` DISABLE KEYS */;
+INSERT INTO `boxes` VALUES (40,6,'F100','In','2017-02-26'),(41,6,'F101','In','2017-02-25'),(42,6,'F200','In','2017-02-26'),(43,6,'F201','In','2017-02-26'),(44,6,'F300','In','2017-02-25'),(45,6,'F301','Out','2017-02-25'),(46,6,'F102','In','2017-02-25'),(47,7,'B100','In','2017-02-25'),(48,7,'B101','Out','2017-02-21'),(49,7,'B102','In','2017-02-24'),(50,7,'B103','Out','2017-02-15'),(51,7,'B333','Out','2017-02-28');
+/*!40000 ALTER TABLE `boxes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `username_canonical` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `email_canonical` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  `salt` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `confirmation_token` varchar(180) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password_requested_at` datetime DEFAULT NULL,
+  `roles` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT '(DC2Type:array)',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_62534E2192FC23A8` (`username_canonical`),
+  UNIQUE KEY `UNIQ_62534E21A0D96FBF` (`email_canonical`),
+  UNIQUE KEY `UNIQ_62534E21C05FB297` (`confirmation_token`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customers`
+--
+
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+INSERT INTO `customers` VALUES (5,'Admin','Brwinów','Admin','admin','admin@mail.com','admin@mail.com',1,NULL,'$2y$13$V6wUrMwEUGCs6E76sniE8O9E9L3f3LcJz.QxgLkGQXoBwTbuUh9Im','2017-02-28 09:31:02',NULL,NULL,'a:1:{i:0;s:10:\"ROLE_ADMIN\";}'),(6,'Foo Company','Warsaw','Foo Company','foo company','foo@mail.com','foo@mail.com',1,NULL,'$2y$13$n9MUfw8et0h4MPGrxEtWy.PuJX.PhLu49f4oBpsrhG0FKwRYF/CBG','2017-02-28 09:30:42',NULL,NULL,'a:0:{}'),(7,'Bar Holding','Berlin','Bar Holding','bar holding','bar@mail.com','bar@mail.com',1,NULL,'$2y$13$4BkD6mZy3zVCHNTZYn75DO6HNDk0oUZtHeiBc06LC5dkvs75yl.HO','2017-02-27 23:35:49',NULL,NULL,'a:0:{}');
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fees`
+--
+
+DROP TABLE IF EXISTS `fees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fees` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `delivery` int(11) NOT NULL,
+  `import` int(11) NOT NULL,
+  `storage` int(11) NOT NULL,
+  `customer_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_A093C16C9395C3F3` (`customer_id`),
+  CONSTRAINT `FK_A093C16C9395C3F3` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fees`
+--
+
+LOCK TABLES `fees` WRITE;
+/*!40000 ALTER TABLE `fees` DISABLE KEYS */;
+INSERT INTO `fees` VALUES (11,100,100,200,7),(12,150,200,300,6);
+/*!40000 ALTER TABLE `fees` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `message` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `thread_id` int(11) DEFAULT NULL,
+  `sender_id` int(11) DEFAULT NULL,
+  `body` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_B6BD307FE2904019` (`thread_id`),
+  KEY `IDX_B6BD307FF624B39D` (`sender_id`),
+  CONSTRAINT `FK_B6BD307FE2904019` FOREIGN KEY (`thread_id`) REFERENCES `thread` (`id`),
+  CONSTRAINT `FK_B6BD307FF624B39D` FOREIGN KEY (`sender_id`) REFERENCES `customers` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `message`
+--
+
+LOCK TABLES `message` WRITE;
+/*!40000 ALTER TABLE `message` DISABLE KEYS */;
+INSERT INTO `message` VALUES (6,4,6,'Ice cream powder brownie. Tart pastry chocolate bar jelly icing. Pie jelly danish gummies toffee. Bear claw biscuit macaroon. Cupcake cheesecake macaroon jelly-o. Cotton candy sweet roll cookie cupcake. Jujubes chupa chups muffin apple pie powder jelly beans cotton candy.','2017-02-26 22:05:32'),(7,5,7,'Powder pie bonbon dessert cheesecake. Macaroon cheesecake chocolate bar. Marzipan bonbon toffee chocolate cake. Gummi bears bonbon pie. Chocolate danish cookie. Gummies cake candy sweet roll sugar plum cookie jujubes sweet. Tart candy lemon drops jelly beans. Marzipan jelly beans topping cupcake pudding caramels gummi bears candy. Topping liquorice sugar plum powder candy. Chupa chups chupa chups cookie pie. Soufflé marshmallow dragée cotton candy fruitcake. Cake biscuit gummies. Cupcake pastry jujubes marzipan dragée sesame snaps. Topping cake cotton candy carrot cake.','2017-02-26 22:12:04'),(8,5,5,'Cupcake cake dragée oat cake. Chocolate bar cotton candy muffin apple pie tootsie roll lollipop bear claw chocolate cake. Sesame snaps topping muffin apple pie cake topping. Chupa chups soufflé biscuit marshmallow soufflé fruitcake soufflé cupcake. Cheesecake jelly beans gingerbread gingerbread.','2017-02-26 22:12:38');
+/*!40000 ALTER TABLE `message` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `message_metadata`
+--
+
+DROP TABLE IF EXISTS `message_metadata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `message_metadata` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `message_id` int(11) DEFAULT NULL,
+  `participant_id` int(11) DEFAULT NULL,
+  `is_read` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_4632F005537A1329` (`message_id`),
+  KEY `IDX_4632F0059D1C3019` (`participant_id`),
+  CONSTRAINT `FK_4632F005537A1329` FOREIGN KEY (`message_id`) REFERENCES `message` (`id`),
+  CONSTRAINT `FK_4632F0059D1C3019` FOREIGN KEY (`participant_id`) REFERENCES `customers` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `message_metadata`
+--
+
+LOCK TABLES `message_metadata` WRITE;
+/*!40000 ALTER TABLE `message_metadata` DISABLE KEYS */;
+INSERT INTO `message_metadata` VALUES (11,6,5,0),(12,6,6,1),(13,7,5,1),(14,7,7,1),(15,8,5,1),(16,8,7,0);
+/*!40000 ALTER TABLE `message_metadata` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `thread`
+--
+
+DROP TABLE IF EXISTS `thread`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `thread` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_by_id` int(11) DEFAULT NULL,
+  `subject` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `is_spam` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_31204C83B03A8386` (`created_by_id`),
+  CONSTRAINT `FK_31204C83B03A8386` FOREIGN KEY (`created_by_id`) REFERENCES `customers` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `thread`
+--
+
+LOCK TABLES `thread` WRITE;
+/*!40000 ALTER TABLE `thread` DISABLE KEYS */;
+INSERT INTO `thread` VALUES (4,6,'Ice cream powder brownie','2017-02-26 22:05:32',0),(5,7,'Powder pie bonbon dessert cheesecake','2017-02-26 22:12:04',0);
+/*!40000 ALTER TABLE `thread` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `thread_metadata`
+--
+
+DROP TABLE IF EXISTS `thread_metadata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `thread_metadata` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `thread_id` int(11) DEFAULT NULL,
+  `participant_id` int(11) DEFAULT NULL,
+  `is_deleted` tinyint(1) NOT NULL,
+  `last_participant_message_date` datetime DEFAULT NULL,
+  `last_message_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_40A577C8E2904019` (`thread_id`),
+  KEY `IDX_40A577C89D1C3019` (`participant_id`),
+  CONSTRAINT `FK_40A577C89D1C3019` FOREIGN KEY (`participant_id`) REFERENCES `customers` (`id`),
+  CONSTRAINT `FK_40A577C8E2904019` FOREIGN KEY (`thread_id`) REFERENCES `thread` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `thread_metadata`
+--
+
+LOCK TABLES `thread_metadata` WRITE;
+/*!40000 ALTER TABLE `thread_metadata` DISABLE KEYS */;
+INSERT INTO `thread_metadata` VALUES (7,4,5,0,NULL,'2017-02-26 22:05:32'),(8,4,6,0,'2017-02-26 22:05:32',NULL),(9,5,5,0,'2017-02-26 22:12:38','2017-02-26 22:12:04'),(10,5,7,0,'2017-02-26 22:12:04','2017-02-26 22:12:38');
+/*!40000 ALTER TABLE `thread_metadata` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-02-28  9:52:11
